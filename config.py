c6a1d001200477f6c9f26bc16ca3c8308c7d56cc
Zihan Jin <101914323+dongyu23@users.noreply.github.com>
2026-03-04 18:10:12 +0800

    111

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 
-API_KEY = "1a8ea1d1c38d45c38ca221b884492a46.lxGhbfNhMEPUmucZ"
+API_KEY = ""
 MODEL_NAME = "glm-4-air" # I'll use a known valid model name first to be safe, or should I trust the user? 
