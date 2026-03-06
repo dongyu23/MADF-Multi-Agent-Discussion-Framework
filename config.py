6c3a79369e91d5d02ca52058e8477caebc466c6e
dongyu23 <1410875946@qq.com>
2026-03-06 18:43:38 +0800

    chore(config): 更新模型名称并添加 SERPAPI API 密钥
    将 MODEL_NAME 从 'glm-4-air' 更新为 'glm-4.6'，并新增 SERPAPI_API_KEY 配置项。

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 
 API_KEY = "1a8ea1d1c38d45c38ca221b884492a46.lxGhbfNhMEPUmucZ"
-MODEL_NAME = "glm-4-air" # I'll use a known valid model name first to be safe, or should I trust the user? 
+MODEL_NAME = "glm-4.6" # I'll use a known valid model name first to be safe, or should I trust the user? 
+SERPAPI_API_KEY="7a08387f3345bff89b1fb06da40d83e724fc3ba6544c751f6fbffe5c1b7ba69a"
